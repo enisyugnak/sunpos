b4ee2f353e805038758b0e8ed46da93f749a48cd
enisyugnak <selflearner@gmail.com>
2022-06-22 23:16:46 +0300

    stay change

---
+++ src/data/stays.js
@@ -0,0 +1,24 @@
+const stays = {
+
+    "accoRow":{
+        "cust_id":"1632750595000",
+        "cust_name":"aaa - hamidin",
+        "row_dates" :["1","2","3","4"],
+        "rooms":[
+            {
+                "room_id":"2",
+                "room_name":"Standart Bungalov",
+                "price":"300",
+                "stay_nights" : [0,1,1,0]
+            },
+            {
+                "room_id":"4",
+                "room_name":"Büyük Bungalov",
+                "price":"450",
+                "stay_nights" : [2,0,0,2]
+            }
+        ]
+
+    }
+
+}
